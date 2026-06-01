c8be573fe432a185ffd298e2d04481aa57901a39
natanael <natanael@keysystems.com.br>
2026-06-01 10:51:54 -0300

    Ajustes

--- backend/KSWeb.Api/obj/Debug/net10.0/KSWeb.Api.AssemblyInfo.cs
+++ backend/KSWeb.Api/obj/Debug/net10.0/KSWeb.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KSWeb.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aab623aa130168cbce26e8debced1da90d27c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697dde1a0e4c455908fafe3659d94cb58f18091e")]
 [assembly: System.Reflection.AssemblyProductAttribute("KSWeb.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KSWeb.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
